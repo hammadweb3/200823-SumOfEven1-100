9117bf1f230483577eaff64f19778bca669cefbc
hammadweb3 <hammad33395@yahoo.com>
2023-08-20 11:31:53 -0700

    sume of even number 1-100

---
+++ indax.js
@@ -0,0 +1,163 @@
+"use strict";
+// // // array
+// // // let names: string[] = ["hammad ahamd", "naveed sarwar", "ali","abubakar"]
+// // // console.log(names[2], names[0],names [1],names[3]);
+// // // let rollnumber: number[]= [10,20,30,40,50,60,70]
+// // // // console.log(rollnumber)
+// // // let sum:number = rollnumber[0] + rollnumber[5]
+// // // console.log(sum)
+// // let names:string[]= ["abu hurrairah","naveed sarwar", "ali"]
+// // // names.push ("ali","waseem2")
+// // // names.pop()
+// // // // console.log(names)
+// // // names.unshift("hammad")
+// // // // console.log(names)
+// // // names.shift("abu hurrairah")
+// // // console.log(names)
+// // names.splice( 2,0, "waseem", "ahmad", "jawad")
+// // console.log(names)
+// // console.log(names.length)
+// // let todo:string[] = ["class attend", "lunch", "dinner"]
+// // todo.splice(todo.length, 0, "show time")
+// // todo.splice(0,1)
+// // console.log(todo)
+// let todo:string[]= ["12", "18", "20", "22", "15", "24", "28", "30"]
+// // todo.slice(todo.length,"show time","52","54","58")
+// var time :number [] = [ 10 ,11 , 12 , 13, 14, 15, 16, 17, 18 , 19]
+// time.splice(time.length,0,20,21,22,23,24)
+// time.splice(1,0,25)
+// function america() {
+//     console.log("pakistan zindabad")
+// }
+// america()
+// Create a function that takes an array, an index, and a value as parameters.
+//  Inside the function, use the splice method to insert the value at the specified index in the array. 
+// Return the modified array.
+// var names:number[] = [12, 13,14,15,16,17,18,19,20]
+// function names1()
+// {
+// names.splice (2,0,21)
+// var result=console.log(names)
+// return result
+// }
+// names.splice (names.length,0,22)
+// names1()
+// let intel: string[] = ["canada", "Australia1", "germany","America", "Europ", "Australia", "India","pakistan", "Afghanistan", "Iran", "kashmir"]
+// console.log(intel.length, "total countries")
+// intel.splice(0,1)
+// console.log(intel)
+// console.log(intel.length, "Remaining countries")
+// console.log(intel)
+/*
+var num1:number = 5
+var num2:number = 10
+num1 +=5
+num1 -= 5
+num1 /=5
+num1 *=5
+var Boss: number = num1**5
+console.log("MyBoss age is", Boss)
+*/
+// let age: number = 25
+// let nationality : string = "pakistani"
+// let result:boolean = age >= 18 && nationality == "pakistani"
+// console.log(result)
+// let age:number = 10
+// let age1:number = 15
+// let result:boolean= age1<=age
+// if(true){
+//     console.log("printsometing")
+//    if (true){
+//     console.log("printsometing1")
+// }
+//    if (false){
+//     console.log("printsometing2")
+// }}
+// {
+//     console.log("printsometing3")
+// // }
+// if (2>5)
+// {
+//     console.log("pakitan zindad")
+// }
+// else {
+//     console.log("pakistan Painabad")
+// }
+// var negativenumber:number =-5
+// if(0<negativenumber)
+// {
+//     console.log (negativenumber, "positive number")
+// }
+// else{
+//     console.log (negativenumber, "negative number")
+// }
+// let age : number = 18
+// let nationality: string = "pakistani"
+// if (12 >= age && nationality == "pakistani")
+// {
+//     console.log("Eligible to cast vote")
+// }
+// else
+// {
+//     console.log("not eligible to cast vote")
+// }
+// var ObtainedMarks:number = 70.50
+// if (ObtainedMarks <=50 )
+// {
+//     console.log ("Student Grade is F")
+// }
+// else if (ObtainedMarks >50 && ObtainedMarks<=60)
+// {
+//     console.log ("Student Grade is E")
+// }
+// else if (ObtainedMarks > 60 && ObtainedMarks <= 70)
+// {
+//     console.log ("Student Grade is D")
+// }
+// else 
+// {
+//     console.log ("Student Grade is C")
+// }
+// var section: string = "E"
+// switch (section) {
+//     case "A" :
+//         console.log("your class timing is 9:00 to 12:00")
+//         break; 
+//         case "B" :
+//             console.log("your class timing is 12:00 to 02:00")
+//             break;
+//         case "C" :
+//                 console.log("your class timing is 02:00 to 03:00")
+//             break;
+//     default:
+//             console.log("your class timing is 4:00 to 06:00")
+//         break;
+// }
+// let num1:string= "A"
+// switch ("A") {
+//     case num1: console.log("your class timing 8:00 AM to 09:00 PM")
+//         break;
+// var i:number = 1
+// while (i<=10)
+// {
+// var J:number= 5 * 1
+// console.log( " 5 *" + i + "=" +J)
+// i =i + 1
+// }
+// for (let index = 10; index < 20; index++)
+// {
+//     console.log ("naveed", index)
+// }
+// var i:number = 10
+// do{
+//     console.log("Naveed");
+//     i ++;
+// }while (i<=30)
+// write a ts program, that calculate sum of even numbers betwween  1 -100
+let sum = 2;
+for (let i = 1; i <= 100; i++) {
+    if (i % 2 == 0) {
+        sum = sum + i;
+        console.log("i", i);
+    }
+}
